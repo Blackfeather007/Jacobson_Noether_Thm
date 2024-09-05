a2d2c2b72d1ee87b0c45ec361a4024d6466fdc65
Suzuka Yu <109365723+Yu-Misaka@users.noreply.github.com>
2024-09-05 16:48:16 +0800

    change \delta

--- JacobsonNoetherThm/CharPAux.lean
+++ JacobsonNoetherThm/CharPAux.lean
@@ -15,7 +15,7 @@ instance : Field k := inferInstance
 #check IntermediateField.adjoin
 
 lemma aux0 [ExpChar k p] :
-  ∀ a : D, a ∉ (algebraMap k D).range → ∃ m ≥ 1, a ^ (p ^ m) ∈ k := sorry
+  ∀ a : D, a ∉ k → ∃ m ≥ 1, a ^ (p ^ m) ∈ k := sorry
 
 def f (a : D) : D → D := fun x ↦ a * x
 
@@ -23,5 +23,19 @@ def g (a : D) : D → D := fun x ↦ x * a
 
 def δ (a : D) : D → D := f a - g a
 
-lemma finial_aux (a : D) (a_nin_k : a ∉ (algebraMap k D).range) :
-  ∃ m ≥ 1, ∀ n ≥ (p ^ m), (δ a) ^ n = 0 := sorry
+lemma finial_aux [ExpChar k p] (a : D) (a_nin_k : a ∉ k) :
+  ∃ m ≥ 1, ∀ n ≥ (p ^ m), (δ ^ n) a = 0 := by
+  obtain ⟨m, hm⟩ := aux0 a a_nin_k (p := p)
+  use m
+  constructor
+  · exact hm.1
+  · intro n hn
+    have inter1 : (f a) * (g a) = (g a) * (f a) := by
+      funext x
+      repeat rw [Pi.mul_apply]
+      unfold f g
+
+      sorry
+    sorry
+
+  sorry
