d26e196a62ee8cac7f50c656a9e9650e3c32762c
yuanyi-350 <kysyy1@126.com>
2024-09-05 11:03:45 +0800

    Update AlgebraInstance.lean

--- JacobsonNoetherThm/AlgebraInstance.lean
+++ JacobsonNoetherThm/AlgebraInstance.lean
@@ -1,3 +1,14 @@
 import Mathlib.Algebra.Ring.Subring.Basic
 
 example : 1 = 1 := by rfl
+
+import Mathlib
+
+variable {R : Type*} [DivisionRing R]
+
+instance : Algebra (Subring.center R) R := by
+  refine Algebra.ofModule ?h₁ ?h₂
+  · intro r x y
+    exact smul_mul_assoc r x y
+  · intro r x y
+    exact mul_smul_comm r x y
