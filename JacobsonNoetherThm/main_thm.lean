0f013af906d25f93d24c1d8d2ea0d8b7332394cc
Blackfeather007 <2100017455@stu.pku.edu.cn>
2024-09-05 14:18:25 +0800

    Update main_thm.lean

--- JacobsonNoetherThm/main_thm.lean
+++ JacobsonNoetherThm/main_thm.lean
@@ -1,14 +1,32 @@
 import JacobsonNoetherThm.AlgebraInstance
 import Mathlib.RingTheory.Algebraic
 import Mathlib.FieldTheory.Separable
+import Mathlib.FieldTheory.Perfect
+import Mathlib.Algebra.CharP.Subring
+
 
 variable {D : Type*} [DivisionRing D]
 
 local notation "k" => (Subring.center D)
 
+
 theorem aux1 [CharP D 0] [Algebra.IsAlgebraic k D] (h : D ≠ k) :
-    ∃ x : D, x ∉ k ∧ IsSeparable k x :=
-  sorry
+    ∃ x : D, x ∉ k ∧ IsSeparable k x := by
+  letI : CharZero k := (CharP.charP_zero_iff_charZero k).mp (by infer_instance)
+  have : ∃ a : D, a ∉ k := by
+
+    sorry
+  obtain ⟨a, ha⟩ := this
+  use a
+  constructor
+  · exact ha
+  · have : Polynomial.Separable (minpoly k a) := by
+      apply Irreducible.separable
+      apply minpoly.irreducible
+      exact Algebra.IsIntegral.isIntegral a
+    exact this
+
+
 
 
 theorem aux2 {p : ℕ} [Fact p.Prime] [CharP D p] [Algebra.IsAlgebraic k D] (h : D ≠ k) :
