ff4be6f46e6db50f61f4071773fcbd560b78c30b
Suzuka Yu <109365723+Yu-Misaka@users.noreply.github.com>
2024-09-05 11:42:19 +0800

    notation

--- JacobsonNoetherThm/main_thm.lean
+++ JacobsonNoetherThm/main_thm.lean
@@ -2,20 +2,23 @@ import JacobsonNoetherThm.AlgebraInstance
 import Mathlib.RingTheory.Algebraic
 import Mathlib.FieldTheory.Separable
 
+variable {D : Type*} [DivisionRing D]
 
-theorem aux1 {R : Type*} [DivisionRing R] [CharP R 0] [Algebra.IsAlgebraic (Subring.center R) R] :
-    ∃ x : R, x ∉ (Subring.center R) ∧ IsSeparable (Subring.center R) x :=
+local notation "k" => (Subring.center D)
+
+theorem aux1 [CharP D 0] [Algebra.IsAlgebraic k D] :
+    ∃ x : D, x ∉ k ∧ IsSeparable k x :=
   sorry
 
 
-theorem aux2 {R : Type*} [DivisionRing R] {p : ℕ} [Fact p.Prime] [CharP R p] [Algebra.IsAlgebraic (Subring.center R) R] :
-    ∃ x : R, x ∉ (Subring.center R) ∧ IsSeparable (Subring.center R) x :=
+theorem aux2 {p : ℕ} [Fact p.Prime] [CharP D p] [Algebra.IsAlgebraic k D] :
+    ∃ x : D, x ∉ k ∧ IsSeparable k x :=
   sorry
 
 
 
-theorem Jacobson_Noether {R : Type*} [DivisionRing R] {p : ℕ} [CharP R p] [Algebra.IsAlgebraic (Subring.center R) R] :
-    ∃ x : R, x ∉ (Subring.center R) ∧ IsSeparable (Subring.center R) x := by
-  rcases @CharP.char_is_prime_or_zero R _ _ _ p _ with h1 | h2
-  · exact @aux2 R _ p ⟨h1⟩ _ _
-  exact @aux1 R _ (CharP.congr p h2) _
+theorem Jacobson_Noether {p : ℕ} [CharP D p] [Algebra.IsAlgebraic k D] :
+    ∃ x : D, x ∉ k ∧ IsSeparable k x := by
+  rcases @CharP.char_is_prime_or_zero D _ _ _ p _ with h1 | h2
+  · exact @aux2 D _ p ⟨h1⟩ _ _
+  exact @aux1 D _ (CharP.congr p h2) _
