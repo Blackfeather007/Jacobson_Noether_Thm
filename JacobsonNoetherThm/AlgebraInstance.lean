9d9429db9f80fc4e2f52008350ff852d984b935b
yuanyi-350 <kysyy1@126.com>
2024-09-05 11:51:10 +0800

    Update AlgebraInstance.lean

--- JacobsonNoetherThm/AlgebraInstance.lean
+++ JacobsonNoetherThm/AlgebraInstance.lean
@@ -1,5 +1,6 @@
 import Mathlib.Algebra.Ring.Subring.Basic
 import Mathlib.Algebra.Algebra.Defs
+import Mathlib.RingTheory.Algebraic
 
 variable {R : Type*} [DivisionRing R]
 
@@ -9,3 +10,10 @@ instance : Algebra (Subring.center R) R := by
     exact smul_mul_assoc r x y
   · intro r x y
     exact mul_smul_comm r x y
+
+instance : Field (Subring.center R) := by
+  exact Subring.instField
+
+example [Algebra.IsAlgebraic (Subring.center R) R] : CharZero (Subring.center R) → IsSeparable (Subring.center R) R := by
+  intro h
+  refine isSeparable_iff.mpr ?_
